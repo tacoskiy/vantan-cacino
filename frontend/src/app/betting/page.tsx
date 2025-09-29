687070a373dad06dab345a6472adb2e8853849cf
haruominomura <haruomi.nomura@gmail.com>
2025-09-29 18:41:26 +0900

    [FE] Update Meta section and Add bottom description (placeholder)

--- frontend/src/app/betting/page.tsx
+++ frontend/src/app/betting/page.tsx
@@ -20,12 +20,25 @@ const BettingPage = () => {
         <CommonButton color="bg-primary shadow-xl floating-glow select-none max-w-120" onClick={bottomSheet.open}><p className="font-bold text-lg">ベットする</p></CommonButton>
       </FloatingContainer>
       <BottomSheet>
-        <div className="px-4">
-          <CommonButton color="bg-primary shadow-xl floating-glow select-none max-w-120" onClick={() => bettingGrid.selectOutsideBet(bettingGrid.getRandomOutsideBet())}><p className="font-bold text-lg">ランダム</p></CommonButton>
+        <div className="px-4 w-full flex flex-col items-center gap-4">
+          <CommonButton color="bg-primary shadow-xl floating-glow select-none" onClick={() => bettingGrid.selectOutsideBet(bettingGrid.getRandomOutsideBet())}><p className="font-bold text-lg">ランダム</p></CommonButton>
         </div>
       </BottomSheet>
       <section className="fixed w-full h-screen px-4 pt-36 pb-120 select-none flex flex-col items-center no-drag overflow-y-scroll overflow-scroll-smooth">
         <BettingContainer/>
+        <div className="px-4 pt-12 w-full max-w-108">
+          <p className="text-text-primary opacity-60">
+            純平の鼻毛は250mまで伸縮可能であり、<br/>
+            その長さを自在に操ることができる。<br/>
+            普段はコンビニでおでんの大根をつまむ程度にしか使わないが、<br/>
+            いざという時には電車のパンタグラフの代わりになったり、<br/>
+            遠くの山頂に引っ掛けて空を駆け抜けることすら可能だ。<br/>
+            ただし弱点もある。<br/>
+            花粉の季節には制御不能になり、<br/>
+            気づけば街灯やアンテナに絡まって、<br/>
+            周囲の人々を大混乱に陥れるのだった――。
+          </p>
+        </div>
       </section>
     </main>
   );
--- frontend/src/components/layouts/BottomSheet.tsx
+++ frontend/src/components/layouts/BottomSheet.tsx
@@ -55,7 +55,7 @@ const BottomSheet = ({ children }:BottomSheetProps) => {
   return (
     <section className="fixed w-full h-screen pointer-events-none z-100">
       <div className={clsx(
-        "pointer-events-auto absolute bottom-0 left-0 w-full h-1/3 bg-bg-modal rounded-t-[44px] transition-all duration-300",
+        "pointer-events-auto absolute bottom-0 left-1/2 -translate-x-1/2 w-full h-1/3 bg-bg-modal rounded-t-[44px] transition-all duration-300 max-w-140",
         bottomSheet.state.isOpen
           ? "translate-y-0"
           : "translate-y-[120%]"
